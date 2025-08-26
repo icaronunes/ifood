7c665be25a0f7477a7d7bdca2782007fbf7eb888
icaro.nunes <icaro.nunes@linx.com.br>
2025-08-26 15:26:21 -0300

    update

--- ifood.py
+++ ifood.py
@@ -5,13 +5,20 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from urllib.parse import urljoin
+import argparse
 
 # URL alvo
 url =  "https://www.ifood.com.br/delivery/sao-paulo-sp/boi-gaucho---unidade-penha-penha/c25cfc08-348f-4a44-ada2-b69e9e55f58f"
+parser = argparse.ArgumentParser(description="Scraper de imagens")
+parser.add_argument("url", help="URL da página para buscar imagens")
+args = parser.parse_args()
+
+url = args.url
+print(f"URL recebida: {url}")
 
 # Configurar Selenium (headless = sem abrir janela do navegador)
 chrome_options = Options()
-# chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--headless")  
 chrome_options.add_argument("--disable-gpu")
 
 driver = webdriver.Chrome(options=chrome_options)
@@ -29,10 +36,10 @@ for i in range(0, last_height, 200):  # Rola a página 5 vezes
 os.makedirs("imagens", exist_ok=True)
 
 # Pega todas as tags <img>
-imgs = driver.find_elements("css selector", "img.dish-card__image")
-
-for i, img in enumerate(imgs, start=1):
-    src = img.get_attribute("src")
+elementos = driver.find_elements("css selector", "a.dish-card--horizontal")
+for i, element in enumerate(elementos, start=1):
+    name = element.find_element("css selector", "h3.dish-card__description").text.strip()
+    src = element.find_element("css selector", "div.dish-card__container-image").find_element("tag name", "img").get_attribute("src")
     if not src:
         continue
 
@@ -41,7 +48,7 @@ for i, img in enumerate(imgs, start=1):
 
     try:
         # Nome do arquivo
-        filename = os.path.join("imagens", f"img_{i}.jpg")
+        filename = os.path.join("imagens", f"{i}_{name.replace(' ', '_')}.jpg")
         # Baixar imagem
         img_data = requests.get(img_url).content
         with open(filename, "wb") as f:
